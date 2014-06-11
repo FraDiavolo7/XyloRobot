9dfb9058bdc33c6b6dd394fa14be568171caf294
FraDiavolo7 <fra.diavolo@gmail.com>
2014-06-11 18:35:17 +0200

    Recactored Code
    MusicRobotV2 refactored code, based on correct MusicRobot

--- Arduino/XyloRobot/XyloRobot.ino
+++ Arduino/XyloRobot/XyloRobot.ino
@@ -1,179 +1,107 @@
 
-#include "Makeblock.h"
-#include <SoftwareSerial.h>
-#include <Wire.h>
+/*************************************************************************
+* File Name          : MusicRobot.ino
+* Author             : Evan
+* Updated            : Evan
+* Version            : V2.0.0
+* Date               : 6/5/2013
+* Description        : 1 Step Motors
+* License            : CC-BY-SA 3.0
+* Copyright (C) 2011 Hulu Robot Technology Co., Ltd. All right reserved.
+* http://www.makeblock.cc/
+**************************************************************************/
+#include <AccelStepper.h>
 
-MeStepperMotor stepper (PORT_1, 0x1);
-MeUltrasonicSensor ultraSensor (PORT_3);
-MeDCMotor kicker (M1);
-MeRGBLed led (PORT_4, DEV1);
-MeLimitSwitch sw (PORT_4, DEV2);
+// Define a stepper and the pins it will use
+AccelStepper stepper(AccelStepper::DRIVER, 12,13); // 12-PUL, 13-DIR
+int limitSW = 8;
+int magnetDrc = 7;
+int magnetPwm = 6;
+int stepMotorEN = 11;
 
-int targetIndex = -1;
-int currentIndex = -1;
-int value;
-long StartPosition = 0;
+int StepNote = 223;
+const unsigned int TempoRead = 12;
+const unsigned int TempoPlay = 10000000;
 
-unsigned int KnockFlag = false;
-unsigned int OneStep = 79;
-int PrevIndex = 0;
-int TargetIndex = 0;
-
-int TimingReadNote = 150;   // Fixed time for the readNote function. 
-int TimingMoveHammer = 150; // Fixed time for the moveHammer function. 
-int TimingKnock = 150;      // Fixed time for the knock function. 
-int TempoStep = 10;         // Time needed to move from one step to another.
-
-void initStepper ()
+void initMotor()
 {
-    stepper.begin (); // initialize stepper driver.
-    stepper.setMicroStep (STP_HALF);
-    stepper.setMaxSpeed (500);
-    stepper.setAcceleration (500);
-    stepper.run ();
-    // output pulse
-    stepper.moveTo ( -10000);
-    while (sw.touched ())
-        ;
-        
-    stepper.stop ();
-    stepper.setCurrentPosition (0);
-    StartPosition = -50; //stepper.currentPosition();
-    stepper.setMaxSpeed (20000);
-    stepper.setAcceleration (10000);
-    stepper.run ();
-    stepper.moveTo (50);
-    while (stepper.currentPosition () != 50)
-        ;
-    Serial.println ("Stepper Begin");
+  stepper.setMaxSpeed(2000);
+  stepper.setAcceleration(15000);
+  stepper.moveTo(-4000);
+  while(!digitalRead(limitSW)) stepper.run(); 
+  stepper.stop();
+  stepper.setCurrentPosition(0);
+  stepper.setMaxSpeed(5000);//5000
 }
 
-void setup ()
+// the setup routine runs once when you press reset:
+void setup()
 {
-    initStepper ();
-    Serial.begin (9600);
-    pinMode (7, OUTPUT);
-    pinMode (6, OUTPUT);
-    digitalWrite (7, HIGH);
-    analogWrite (6, 0);
-    pinMode (6, INPUT);
-    led.setNumber (31);
+  // initialize the digital pin as an output.
+  pinMode(limitSW, INPUT);
+  pinMode(magnetDrc, OUTPUT);
+  pinMode(magnetPwm, OUTPUT);
+  pinMode(stepMotorEN, OUTPUT);
+  digitalWrite(stepMotorEN, HIGH);
+  digitalWrite(magnetDrc, LOW);
+  digitalWrite(magnetPwm, 0);
+  initMotor();
+  Serial.begin(9600);
+  Serial.print("start:");Serial.println(digitalRead(limitSW));
 }
 
-/**
- * This function is responsible of the reading of the Note to play
- * This function always take the same time to execute.
-**/
+unsigned int Note = 0;
+boolean receiveFlag = true;
+boolean knockFlag = false;
+// the loop routine runs over and over again forever:
+void loop()
+{
+    Note = readNote ();
+    
+    if ( Note != 0 )
+        playNote (Note);
+}
+
 int readNote ()
 {
-    int IndexNote = 0;
-    
-    if (Serial.available ())
+    int Note = 0;
+
+    if ( Serial.available() && receiveFlag )
     {
-        char temp = Serial.read ();
-        if (temp < 0x12)
+        unsigned char rxTemp = Serial.read ();
+        if ( rxTemp < 0x12 ) //0 ~ 17 scale
         {
-            IndexNote = temp;
-            KnockFlag = true;
+            Note = rxTemp * StepNote;
         }
-    }
+    } 
     else
     {
-        value = ultraSensor.distanceCm ();
-        if (value < 70)
-        {
-            if (value < 10)
-            {
-                IndexNote = 1;
-            }
-            else if (value < 20)
-            {
-                IndexNote = 2;
-            }
-            else if (value < 30)
-            {
-                IndexNote = 3;
-            }
-            else if (value < 40)
-            {
-                IndexNote = 4;
-            }
-            else if (value < 50)
-            {
-                IndexNote = 5;
-            }
-            else if (value < 60)
-            {
-                IndexNote = 6;
-            }
-            else if (value < 70)
-            {
-                IndexNote = 7;
-            }
-            else
-            {
-                IndexNote = 16;
-            }
-
-//            Serial.printf ("%d,%d\n", value, IndexNote);
-        }
-
-        if (IndexNote < 16)
-        {
-            KnockFlag = true;
-        }
+        delayMicroseconds (TempoRead);
     }
 
-    return IndexNote;
+    return Note;
 }
 
-/**
- * This function is responsible of the moving of the hammer.
- * This function always take the same time to execute.
-**/
-void moveHammer ()
+void playNote ( int Note )
 {
-    int Tempo = TimingMoveHammer;
+    int BeginMove = micros ();
+    stepper.moveTo ( Note );
     
-    if ( (TargetIndex >= 0) && (TargetIndex != PrevIndex) )
+    while ( stepper.currentPosition() != Note )
+        stepper.run(); 
+  
+    if ( stepper.currentPosition() == Note )
     {
-        int StepPos = StartPosition + TargetIndex * OneStep;
-        stepper.moveTo (StepPos);
-        Tempo -= abs (TargetIndex - PrevIndex) * TempoStep;
-        PrevIndex = TargetIndex;
+            analogWrite(magnetPwm, 255);
+            delay(50);
+            analogWrite(magnetPwm, 0);
+            Serial.write(1);
     }
-    
-    delayMicroseconds (Tempo);
-}
-
-/**
- * This is a copy of the kickoff function
- * This function is commanding the Hammer to knock
-**/
-void knock ()
-{
-    if ( KnockFlag )
-    {
-        KnockFlag = false;
-        Serial.println ("kick");
-        pinMode (6, OUTPUT);
-        analogWrite (6, 100);
-        delay (50);
-        analogWrite (6, 0);
-        pinMode (6, INPUT);
-    }
-    else
-    {
-        delayMicroseconds (TimingKnock);
-    }
-}
-
-int ledFlag = true;
-void loop ()
-{
-    int TargetIndex = readNote ();
-    
-    moveHammer ();
-
-    knock ();
+    /*
+    int EndMove = micros ();
+    if ( EndMove > BeginMove )
+        delayMicroseconds ( TempoPlay - ( EndMove - BeginMove ) );
+    else*/ 
+        delayMicroseconds ( TempoPlay );
+   
 }
